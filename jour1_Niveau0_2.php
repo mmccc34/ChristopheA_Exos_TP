934e10816ffa7343ef97f2ec9a7a002cec333480
mmccc34 <mmccc34@icloud.com>
2024-11-25 10:20:45 +0100

    jour1_Niveau0_2.php

---
+++ jour1_Niveau0_2.php
@@ -0,0 +1,13 @@
+<?php
+
+function  checkAge ($age) {
+
+    if ($age < 18) {
+        echo 'Vous êtes mineur'.PHP_EOL;
+    
+    }
+    else {
+        echo 'Vous êtes Majeur' . PHP_EOL;
+    }
+}
+
