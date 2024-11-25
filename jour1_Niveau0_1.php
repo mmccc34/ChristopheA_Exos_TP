10c27806e839d85b8b177f0616f1aed355417073
mmccc34 <mmccc34@icloud.com>
2024-11-25 10:17:22 +0100

    jour1_Niveau0_1

---
+++ jour1_Niveau0_1.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 'texte';
+$b = 10;
+$c = true;
+
+echo 'je suis un texte : ' . $a . PHP_EOL;
+echo 'je suis un nombre : ' . $b . PHP_EOL;
+echo 'je suis un booléen : ' . $c . PHP_EOL;
+
+
+for ($i = 1; $i <= 20; $i++) {
+    echo $i . PHP_EOL;
+}
