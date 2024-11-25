edf6c9d47e36719dd25b1e37f88caf9fcfea8a6f
mmccc34 <mmccc34@icloud.com>
2024-11-25 10:42:53 +0100

    jour1_Niveau0_22.php

---
+++ jour1_Niveau0_22.php
@@ -0,0 +1,28 @@
+<?php
+
+$prenom = '';
+
+if (isset($_POST['prenom']) && !empty($_POST['prenom'])) {
+    $prenom = htmlspecialchars($_POST['prenom']);
+    echo 'Bienvenue ' . $prenom . ' !<br>';
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prénom</title>
+</head>
+<body>
+    <form action="" method="POST">
+
+    <label for="prenom">Entrez votre prénom</label>
+    <input type="text"  id="prenom" name="prenom">
+    <input type="submit" value="Envoyez">
+    </form>
+</body>
+</html>
